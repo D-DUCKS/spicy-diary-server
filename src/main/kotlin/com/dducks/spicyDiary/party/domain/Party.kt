259c9080ebfce8f327cdc423641c0da3f78723ae
SeonheeKim <dnwdjqldb@naver.com>
2020-12-02 02:37:13 +0900

    revert party

--- src/main/kotlin/com/dducks/spicyDiary/party/domain/Party.kt
+++ src/main/kotlin/com/dducks/spicyDiary/party/domain/Party.kt
@@ -1,20 +1,20 @@
-//package com.dducks.spicyDiary.party.domain
-//
-//import java.time.LocalDateTime
-//import javax.persistence.*
-//
-//@Entity
-//class Party(wishListId: WishListId, dateAndTime: LocalDateTime, hostId: HostId) {
-////    val id
-//
-//    @Embedded
-//    @AttributeOverride(name = "id", column = Column(name = "wishlist_id"))
-//    val wishListId:WishListId = wishListId
-//
-//    @Embedded
-//    val dateAndTime:LocalDateTime = dateAndTime
-//
-//    @Embedded
-//    @AttributeOverride(name = "id", column = Column(name = "host_id"))
-//    val hostId:HostId = hostId
-//}
+package com.dducks.spicyDiary.party.domain
+
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+class Party(wishListId: WishListId, dateAndTime: LocalDateTime, hostId: HostId) {
+//    val id
+
+    @Embedded
+    @AttributeOverride(name = "id", column = Column(name = "wishlist_id"))
+    val wishListId:WishListId = wishListId
+
+    @Embedded
+    val dateAndTime:LocalDateTime = dateAndTime
+
+    @Embedded
+    @AttributeOverride(name = "id", column = Column(name = "host_id"))
+    val hostId:HostId = hostId
+}
